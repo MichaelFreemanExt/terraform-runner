4eeddb5747c357f4de723e901496c429ba0a618a
DESKTOP-OHSHGG1\mikec <michael.freeman.ext@lilium.com>
2021-08-18 14:48:45 +0100

    asd

--- image/entrypoints/entrypoint.ps1
+++ image/entrypoints/entrypoint.ps1
@@ -26,23 +26,13 @@ try {
         SourceDirectory = "$currentDirectory"
         WorkingDirectory = "$terraformFiles"
     }   
-    Resolve-PipelineDirectory @resolve_params
+    Resolve-WorkflowDirectory @resolve_params
 
     Write-Host "Resolved Working Directory $Env:ResolvedWorkingDirectory"
 
     #write the terraform backend
     $backendConfigHashTable = ConvertFrom-StringData -StringData $backendConfig
-    Write-TfBackend $backendConfigHashTable    
-
-    Write-Host "looks like everything is good!"
-    Write-Host "PS Module Paths $Env:PSModulePath"
-    $childItem = Get-ChildItem "/opt/microsoft/powershell/7-lts/Modules"
-    Write-Host "Module Children $childItem"
-    $childItem = Get-ChildItem "/opt/microsoft/powershell/7-lts/Modules/RunTools"
-    Write-Host "RunTools Module Children $childItem"
-    $installedModules = Get-Module -ListAvailable
-    Write-Host "Available Modules $installedModules"
-
+    Write-TfBackend $backendConfigHashTable
 
     Add-PrComment "Michael"
     & "terraform" version
