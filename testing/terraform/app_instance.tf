60d59fa3153c93bef68324117f477c15ee05b617
DESKTOP-OHSHGG1\mikec <michael.freeman.ext@lilium.com>
2021-08-23 13:10:26 +0100

    asd

--- testing/terraform/app_instance.tf
+++ testing/terraform/app_instance.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "fakeecrtest" {
-  name                 = "fakeecrtest"
+  name                 = "fakeecrtest1"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
